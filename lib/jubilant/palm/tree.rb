3d5c7fb699a391bb5e3a637741d97a11983473e9
DESKTOP-U6KI6OJ\Michael Polzin <polzin13@gmail.com>
2018-02-23 22:18:37 -0600

    Create base project

---
+++ lib/jubilant/palm/tree.rb
@@ -0,0 +1,9 @@
+require "jubilant/palm/tree/version"
+
+module Jubilant
+  module Palm
+    module Tree
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jubilant/palm/tree/version.rb
@@ -0,0 +1,7 @@
+module Jubilant
+  module Palm
+    module Tree
+      VERSION = "0.1.0"
+    end
+  end
+end
