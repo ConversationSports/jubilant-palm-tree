3cb433c6ee0522743b80d62d56f91911fdbf1792
DESKTOP-U6KI6OJ\Michael Polzin <polzin13@gmail.com>
2018-02-23 22:20:28 -0600

    Remove a bunch of garbage

--- lib/jubilant/palm/tree.rb
+++
@@ -1,9 +0,0 @@
-require "jubilant/palm/tree/version"
-
-module Jubilant
-  module Palm
-    module Tree
-      # Your code goes here...
-    end
-  end
-end
--- lib/jubilant/palm/tree/version.rb
+++
@@ -1,7 +0,0 @@
-module Jubilant
-  module Palm
-    module Tree
-      VERSION = "0.1.0"
-    end
-  end
-end
